4031357ad8b3e67bbda1e6f5baf59a274e68fcc7
scadoshi <scottyfermo@hotmail.com>
2025-11-13 10:16:42 -0800

    2024 day 1 done

--- src/lib/year_2015/day_01/part_01.rs
+++ src/lib/year_2015/day_01/part_01.rs
@@ -1,5 +1,8 @@
 #[allow(dead_code)]
-pub fn part_01(_input: &str) -> i32 {
-    // TODO: implement
-    0
+pub fn part_01(input: &(Vec<i32>, Vec<i32>)) -> i32 {
+    let (vec1, vec2) = input;
+    vec1.iter()
+        .zip(vec2)
+        .map(|(num1, num2)| (num1 - num2).abs())
+        .sum()
 }
