1bf11349d470a6d72a89f679f2f9245a565caf1e
scadoshi <scottyfermo@hotmail.com>
2025-11-30 09:49:38 -0800

    more sensible solution to 18 3

--- src/lib/year_2018/day_03/part_02.rs
+++ src/lib/year_2018/day_03/part_02.rs
@@ -1,14 +1,17 @@
-use crate::year_2018::day_03::claim::Claim;
+use crate::year_2018::day_03::{claim::Claim, point::Point};
 
 pub fn part_02(input: &[Claim]) -> Option<usize> {
-    for (i, claim) in input.iter().enumerate() {
-        if !input
+    let claims: Vec<(usize, Vec<Point>)> = input.iter().fold(Vec::new(), |mut claims, claim| {
+        claims.push((claim.id, claim.to_points()));
+        claims
+    });
+    for (id, points) in claims.iter() {
+        if !claims
             .iter()
-            .enumerate()
-            .filter(|(j, _)| i != *j)
-            .any(|(_, other)| claim.overlaps_with(*other))
+            .filter(|(other_id, _)| id != other_id)
+            .any(|(_, other_points)| points.iter().any(|point| other_points.contains(point)))
         {
-            return Some(claim.id);
+            return Some(*id);
         }
     }
     None
