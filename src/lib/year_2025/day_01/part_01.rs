07645c2acf0fc8cf08637ef88f71b993c983fe17
scadoshi <scottyfermo@hotmail.com>
2025-12-01 10:40:16 -0800

    like my laundry

--- src/lib/year_2025/day_01/part_01.rs
+++ src/lib/year_2025/day_01/part_01.rs
@@ -1,15 +1,16 @@
 #[allow(dead_code)]
 pub fn part_01(input: &[isize]) -> isize {
-    let mut p = 50;
-    let mut t = 0;
-    for num in input {
-        p = (p + num) % 100;
-        if p < 0 {
-            p += 100;
-        }
-        if p == 0 {
-            t += 1;
-        }
-    }
-    t
+    input
+        .iter()
+        .fold((50, 0), |(mut p, mut t), num| {
+            p = (p + num) % 100;
+            if p < 0 {
+                p += 100;
+            }
+            if p == 0 {
+                t += 1;
+            }
+            (p, t)
+        })
+        .1
 }
