75c200fc2da2decf9ea124abfcc1c499efef8e4f
scadoshi <scottyfermo@hotmail.com>
2025-12-01 11:21:50 -0800

    dynamic range building unecessary

--- src/lib/year_2025/day_01/part_02.rs
+++ src/lib/year_2025/day_01/part_02.rs
@@ -3,8 +3,7 @@ pub fn part_02(input: &[isize]) -> isize {
     input
         .into_iter()
         .fold((50, 0), |(mut p, mut t), num| {
-            let rng = if *num > 0 { 0..*num } else { *num..0 };
-            for _ in rng {
+            for _ in 0..num.abs() {
                 p = (p + num.signum()) % 100;
                 if p < 0 {
                     p += 100;
