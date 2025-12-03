06e54da9dc8b5f31303bd0adab7b43615ba66101
scadoshi <scottyfermo@hotmail.com>
2025-12-02 17:14:48 -0800

    iter chain ooh

--- src/lib/year_2025/day_02/part_02.rs
+++ src/lib/year_2025/day_02/part_02.rs
@@ -1,19 +1,18 @@
 use std::ops::RangeInclusive;
 
 pub fn part_02(input: &[RangeInclusive<usize>]) -> usize {
-    let mut total = 0;
-    for range in input {
-        'ids: for id in range.clone() {
+    input.iter().fold(0, |mut total, range| {
+        range.clone().for_each(|id| {
             let id_str = id.to_string();
             let bytes = id_str.as_bytes();
             for i in 1..=id.to_string().len() / 2 {
                 let first = &bytes[..i];
-                if bytes.chunks(i).all(|chunk| chunk == first) {
+                if !bytes.chunks(i).any(|chunk| first != chunk) {
                     total += id;
-                    continue 'ids;
+                    break;
                 }
             }
-        }
-    }
-    total
+        });
+        total
+    })
 }
