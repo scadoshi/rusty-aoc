2ac637c458201e2a8454c6fc67ea1cbbae2852d9
scadoshi <scottyfermo@hotmail.com>
2025-12-16 16:01:46 -0800

    17 4 done

--- src/lib/year_2017/day_04/part_01.rs
+++ src/lib/year_2017/day_04/part_01.rs
@@ -1,7 +1,7 @@
 use std::collections::HashSet;
 
-pub fn part_01(input: &[&'static str]) -> usize {
-    input.into_iter().fold(0usize, |acc, line| {
+pub fn part_01(input: &[&'static str]) -> u16 {
+    input.into_iter().fold(0u16, |acc, line| {
         let mut seen = HashSet::<&[u8]>::new();
         for word in line.split_whitespace().map(|w| w.as_bytes()) {
             if seen.contains(word) {
